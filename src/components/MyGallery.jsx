7d9596c698b23616cb1e9738d268f0729304c72a
Nat-Nigro <natalenigro15@gmail.com>
2024-01-10 16:19:07 +0100

    commit pomeridiano

--- src/components/MyGallery.jsx
+++ src/components/MyGallery.jsx
@@ -1,41 +1,42 @@
-import { Component } from "react";
 import { Container, Row } from "react-bootstrap";
 import SingleMovie from "./SingleMovie";
+import { useEffect, useState } from "react";
 
-class MyGallery extends Component {
-  state = {
-    filmData: [],
-  };
+const MyGallery = (props) => {
+  // state = {
+  //   filmData: [],
+  // };
+
+  const [filmaData, setFilmData] = useState([]);
+
   filmDataFetch = async () => {
     const apiKey = "967961a2";
 
     try {
-      const response = await fetch(`http://www.omdbapi.com/?s=${this.props.searchValue}&apikey=${apiKey}`);
+      const response = await fetch(`http://www.omdbapi.com/?s=${props.searchValue}&apikey=${apiKey}`);
 
       if (!response.ok) {
         throw new Error("ERROR");
       }
       const filmDataObj = await response.json();
-      this.setState({ filmData: filmDataObj.Search });
+      setFilmData({ filmData: filmDataObj.Search });
     } catch (error) {
       console.error("Errore dati", error.message);
     }
   };
-  componentDidMount() {
-    this.filmDataFetch();
-  }
-  render() {
-    return (
-      <Container className="my-2" fluid>
-        <h3 className="text-white">{this.props.searchValue.toUpperCase()}</h3>
-        <Row className="row-cols-md-3 row-cols-xl-6  gy-2">
-          {this.state.filmData.slice(0, 6).map((movie, index) => (
-            <SingleMovie key={index} imageUrl={movie.Poster} />
-          ))}
-        </Row>
-      </Container>
-    );
-  }
-}
+  useEffect(() => {
+    filmDataFetch();
+  }, []);
 
+  return (
+    <Container className="my-2" fluid>
+      <h3 className="text-white">{props.searchValue.toUpperCase()}</h3>
+      <Row className="row-cols-md-3 row-cols-xl-6  gy-2">
+        {setFilmDatailmData.slice(0, 6).map((movie, index) => (
+          <SingleMovie key={index} imageUrl={movie.Poster} />
+        ))}
+      </Row>
+    </Container>
+  );
+};
 export default MyGallery;
